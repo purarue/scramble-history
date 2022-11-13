4ace0d0fe14571b7b63041aa076dfba3b45ec7e2
Sean Breckenridge <seanbrecke@gmail.com>
2022-11-12 20:07:31 -0800

    add average_parser, twistytimer serializer

--- manual_scripts/manual_solves.py
+++ manual_scripts/manual_solves.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
-from typing import NamedTuple, Optional
+import os
+from typing import NamedTuple, Optional, List
+from decimal import Decimal
 from datetime import datetime
 
 import click
 import autotui.shortcuts
+
+from scramble_history.twistytimer import Solve, serialize_solves
+from scramble_history.average_parser import parse_average
 
 
 class RawComment(NamedTuple):
@@ -14,7 +19,7 @@ class RawComment(NamedTuple):
     scrambles: str
     when: datetime
     method: Optional[str]
-    comment: str
+    comment: Optional[str]
 
     @staticmethod
     def attr_use_values() -> dict:
@@ -29,17 +34,48 @@ def edit_in_vim(text: str | None) -> str | None:
     return m if m is None else m.strip()
 
 
+this_dir = os.path.abspath(os.path.dirname(__file__))
+manual_solves_file = os.path.join(this_dir, "./manual_solves.json")
+
+
 @click.command()
 @click.argument("CMD", type=click.Choice(["loop", "parse"]))
 def main(cmd: str):
+    """
+    loop - repeatedly prompts to input data
+    parse - parses those into the twistytimer csv export
+    """
     if cmd == "loop":
         while True:
-            print("NEW SOLVE")
-            autotui.shortcuts.load_prompt_and_writeback(
-                RawComment, "./manual_solves.json"
-            )
+            autotui.shortcuts.load_prompt_and_writeback(RawComment, manual_solves_file)
     else:
-        pass
+        parsed: List[Solve] = []
+        for m in autotui.shortcuts.load_from(RawComment, manual_solves_file):
+            solves = parse_average(m.times)
+            scram = [scr.strip() for scr in m.scrambles.splitlines()]
+            assert len(solves) >= 1, f"no times {solves} {m}"
+            assert len(scram) == len(
+                solves
+            ), f"solves didnt match scramble length {scram} {solves}"
+            if m.is_group:
+                assert len(solves) > 1, f"for group {m}, solve length <= 1"
+            for scr, sl in zip(scram, solves, strict=True):
+                # the scramble/category probably has to be fixed
+                # a bit here, but that can be done with a merge
+                # at a higher level/cleaned up manually
+                parsed.append(
+                    Solve(
+                        scramble=scr,
+                        puzzle=m.event,
+                        category=m.method or "",
+                        dnf=sl == "DNF",
+                        time=Decimal("0") if isinstance(sl, str) else sl,
+                        penalty=Decimal("0"),
+                        when=datetime.fromtimestamp(m.when.timestamp()),
+                        comment=m.comment.strip() if m.comment is not None else "",
+                    )
+                )
+        click.echo(serialize_solves(parsed), nl=False)
 
 
 if __name__ == "__main__":
