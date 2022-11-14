9a7eefc3cdba3b2c2d0b8b3e056b1b6567263282
Sean Breckenridge <seanbrecke@gmail.com>
2022-11-14 01:58:19 -0800

    v0.1.6: add more query opts, best times for stats

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ reqs = Path("requirements.txt").read_text().strip().splitlines()
 pkg = "scramble_history"
 setup(
     name=pkg,
-    version="0.1.5",
+    version="0.1.6",
     url="https://github.com/seanbreckenridge/scramble-history",
     author="Sean Breckenridge",
     author_email="seanbrecke@gmail.com",
