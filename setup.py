6841cd2e88dcde6bb20983c670f0c23aa5810991
Sean Breckenridge <seanbrecke@gmail.com>
2022-11-15 14:36:28 -0800

    v0.1.9: add graph annotation

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ reqs = Path("requirements.txt").read_text().strip().splitlines()
 pkg = "scramble_history"
 setup(
     name=pkg,
-    version="0.1.8",
+    version="0.1.9",
     url="https://github.com/seanbreckenridge/scramble-history",
     author="Sean Breckenridge",
     author_email="seanbrecke@gmail.com",
